d88757aa5040961865ec3c42e8afa0d13f2c3510
Capril1042 <Caprilewis1042@gmail.com>
2017-11-06 14:11:48 -0500

    'Done'

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
- Volume in drive C has no label.
- Volume Serial Number is 504B-8976
+console.log('hello world!');
 
- Directory of C:\Users\Capri\Documents\GitHub\JavaScript-I-Mini
-
-11/06/2017  01:59 PM    <DIR>          .
-11/06/2017  01:59 PM    <DIR>          ..
-11/06/2017  01:58 PM               721 README.md
-11/06/2017  01:59 PM                 0 script.js
-               2 File(s)            721 bytes
-               2 Dir(s)  519,849,840,640 bytes free
